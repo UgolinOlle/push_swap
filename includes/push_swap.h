c30ea052353bcf74209dc025d634351e1c22aef9
Ugolin Ollé <hello@ugolin-olle.com>
2023-12-12 21:44:31 +0700

    feat: 🎸 adding libs and struct

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -3,24 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: uolle <uolle>                              +#+  +:+       +#+        */
+/*   By: ugolin-olle <ugolin-olle@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 22:44:58 by uolle             #+#    #+#             */
-/*   Updated: 2023/12/04 22:47:08 by uolle            ###   ########.fr       */
+/*   Updated: 2023/12/11 10:26:58 by ugolin-olle      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
-#define PUSH_SWAP_H
+# define PUSH_SWAP_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+# include "../libs/includes/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-typedef struct s_list {
-  int value;
-  int index;
-  struct s_list *next;
-} t_list;
+typedef struct s_stack
+{
+	int				value;
+	int				index;
+	int				pos;
+	int				target_pos;
+	int				cost_a;
+	int				cost_b;
+	struct s_stack	*next;
+}					t_stack;
 
 #endif
