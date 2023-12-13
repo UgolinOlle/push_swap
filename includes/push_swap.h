9ae865e65dc7183c1829df916ad496257cdfe4c3
Ugolin Ollé <hello@ugolin-olle.com>
2023-12-13 13:23:33 +0700

    style: 💄 formatting

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -11,22 +11,21 @@
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#define PUSH_SWAP_H
 
-# include "../libs/includes/libft.h"
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
+#include "../libs/includes/libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-typedef struct s_stack
-{
-	int				value;
-	int				index;
-	int				pos;
-	int				target_pos;
-	int				cost_a;
-	int				cost_b;
-	struct s_stack	*next;
-}					t_stack;
+typedef struct s_stack {
+  int value;
+  int index;
+  int pos;
+  int target_pos;
+  int cost_a;
+  int cost_b;
+  struct s_stack *next;
+} t_stack;
 
 #endif
