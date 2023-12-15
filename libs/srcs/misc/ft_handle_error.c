f697d22c81aac0f5180dad3b9e7102e4fc70f635
Ugolin Ollé <hello@ugolin-olle.com>
2023-12-16 02:19:33 +0700

    fix: 🐛 change ouput to correct err

--- libs/srcs/misc/ft_handle_error.c
+++ libs/srcs/misc/ft_handle_error.c
@@ -3,18 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_handle_error.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ugolin-olle <ugolin-olle@student.42.fr>    +#+  +:+       +#+        */
+/*   By: uolle <uolle@student.42bangkok.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 14:00:08 by ugolin-olle       #+#    #+#             */
-/*   Updated: 2023/11/25 14:51:48 by ugolin-olle      ###   ########.fr       */
+/*   Updated: 2023/12/16 00:57:24 by uolle            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/libft.h"
+#include <unistd.h>
 
-void	ft_handle_error(char *content)
-{
-	ft_putstr_fd(content, STDOUT_FILENO);
-	ft_putstr_fd("\n", STDOUT_FILENO);
-	exit(EXIT_FAILURE);
+void ft_handle_error(char *content) {
+  ft_putstr_fd(content, STDERR_FILENO);
+  exit(EXIT_FAILURE);
 }
