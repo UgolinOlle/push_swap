eaf3e8ed0b0772839b162e50ec59b9bea5964ca3
Ugolin Ollé <hello@ugolin-olle.com>
2023-12-25 21:46:07 +0100

    docs(header): update comment

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: uolle <uolle@student.42bangkok.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 22:44:58 by uolle             #+#    #+#             */
-/*   Updated: 2023/12/17 17:36:17 by uolle            ###   ########.fr       */
+/*   Updated: 2023/12/24 15:15:40 by uolle            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@
 #include <unistd.h>
 
 // -------------------------------------------------------------------------------------
-// //
 
 // -- STRUCTURE
 typedef struct s_stack {
@@ -34,9 +33,8 @@ typedef struct s_stack {
 } t_stack;
 
 // -------------------------------------------------------------------------------------
-// //
 
-// -- UTILS
+// -- Utils for common
 int ft_stack_sorted(t_stack *stack);
 int ft_duplicate_check(char **argv);
 int ft_duplicate_sign_check(char **argv);
@@ -44,40 +42,47 @@ char **ft_join_split(char **argv);
 int ft_count_values(char **values);
 int ft_is_digit(char *str);
 
-// -- UTILS STACK
+// -- Utils for stack
 void ft_push_bottom(t_stack **head, t_stack *new_node);
 t_stack *ft_get_last_stack(t_stack *stack);
 int ft_stack_len(t_stack *stack);
 void ft_print_stack(t_stack *stack);
 
-// -- INIT
+// -- Initialization
 void ft_new_stack(t_stack **head, int nbr);
 void ft_indexation(t_stack *stack, int ssize);
 void ft_init_stack(int len_values, char **values, t_stack **stack);
 
-// -- SORT
+// -- Sort
 void ft_sort_three(t_stack **stack_a);
 
-// -- BIG SORT
+// -- Radix
+void ft_radix_sort(t_stack **stack_a, t_stack **stack_b);
+
+// -- Big sort
 void ft_big_sort(t_stack **stack_a, t_stack **stack_b);
+
+// -- Big sort utils
+void ft_find_target(t_stack **stack_a, t_stack **stack_b);
+void ft_find_cost(t_stack **stack_a, t_stack **stack_b);
 
 // -------------------------------------------------------------------------------------
 
-// -- PUSH
+// -- Push
 void ft_pa(t_stack **stack_a, t_stack **stack_b);
 void ft_pb(t_stack **stack_a, t_stack **stack_b);
 
-// -- ROTATE
+// -- Rotate
 void ft_ra(t_stack **stack);
 void ft_rb(t_stack **stack);
 void ft_rr(t_stack **stack_a, t_stack **stack_b);
 
-// -- REVERSE ROTATE
+// -- Reverse rotate
 void ft_rra(t_stack **stack);
 void ft_rrb(t_stack **stack);
 void ft_rrr(t_stack **stack_a, t_stack **stack_b);
 
-// -- SWAP
+// -- Swap
 void ft_sa(t_stack **stack_a);
 void ft_sb(t_stack **stack_b);
 void ft_ss(t_stack **stack_a, t_stack **stack_b);
